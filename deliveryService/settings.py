5e884254abf811d450f15027e39ec6c6c20a570c
FreeCreativ <okaforjonathant@outlook.com>
2024-11-13 11:48:34 +0100

    created the models

--- deliveryService/settings.py
+++ deliveryService/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wv#o-fg_)#u!n)8hz@3%my6-de&ww&*=m_+834z04eps@_4*jp'
+# Get SECRET_KEY from environment variable
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+# Optional: Raise an error if SECRET_KEY is not set
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY environment variable not set!")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'service.apps.ServiceConfig',
     'business.apps.BusinessConfig',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
